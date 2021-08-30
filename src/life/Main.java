8d641f0b15f74841f9de3bc0ce6c492bdc4ba3f9
kerness <maciej.bak260@gmail.com>
2021-08-30 22:03:34 +0200

    counting neighbors and logic done

--- src/life/Main.java
+++ src/life/Main.java
@@ -14,17 +14,15 @@ public class Main {
         universe.setUniverseProperties();
         universe.populate();
         universe.showUniverse(universe.getCurrentUniverse());
-
         universe.evolve();
-
         universe.showUniverse(universe.getNextGeneration());
-
-        universe.getGenAlg().evolveOneGeneration();
+        universe.getGenAlg().createTmpGen();
         universe.getGenAlg().printGenTmp();
+        universe.getGenAlg().evolveOneGeneration();
     }
 }
 
- class Universe {
+class Universe {
     private final Scanner scanner = new Scanner(System.in);
     private int size;
     private int seed;
@@ -32,25 +30,23 @@ public class Main {
     private char[][] nextGeneration;
     private final generationAlgorithm genAlg = new generationAlgorithm();
 
-
     public void setSize(int size) {
         this.size = size;
     }
-    public void setSeed(int seed) {
-        this.seed = seed;
+    public void setSeed(int seed) { this.seed = seed; }
+
+    public char[][] getCurrentUniverse() {
+        return currentUniverse;
+    }
+    public char[][] getNextGeneration() {
+        return nextGeneration;
     }
 
+    generationAlgorithm getGenAlg() {
+        return this.genAlg;
+    }
 
-
-     public char[][] getCurrentUniverse() {
-         return currentUniverse;
-     }
-
-     public char[][] getNextGeneration() {
-        return nextGeneration;
-     }
-
-     void setUniverseProperties(){
+    void setUniverseProperties() {
         System.out.println("setUniverseProp");
         setSize(scanner.nextInt());
         setSeed(scanner.nextInt());
@@ -65,8 +61,7 @@ public class Main {
             for (int j = 0; j < size; j++) {
                 if (random.nextBoolean()) {
                     currentUniverse[i][j] = 'O';
-                }
-                else {
+                } else {
                     currentUniverse[i][j] = '-';
                 }
             }
@@ -74,12 +69,11 @@ public class Main {
     }
 
     void evolve() {
-        genAlg.generateGeneration(this.currentUniverse, this.size);
+        genAlg.copyCurrToNext(this.currentUniverse, this.size);
         nextGeneration = genAlg.getNextGen();
     }
 
     // i - columns ; j - rows
-
     public void showUniverse(char[][] uni) {
         System.out.println("Showing universe: ");
         for (int i = 0; i < size; i++) {
@@ -90,44 +84,37 @@ public class Main {
             System.out.println();
         }
     }
-
-    generationAlgorithm getGenAlg() {
-        return this.genAlg;
-    }
 }
 
 
- class generationAlgorithm {
+class generationAlgorithm {
 
     private int neighbors;
     private char[][] currGen;
     private char[][] nextGen;
-
     private char[][] tmpGen;
 
-     char[][] getNextGen() {
-         return this.nextGen;
-     }
+    char[][] getNextGen() {
+        return this.nextGen;
+    }
+    char[][] getCurrGen() {
+        return this.currGen;
+    }
+    public char[][] getTmpGen() {
+        return tmpGen;
+    }
 
-     char[][] getCurrGen() {
-         return  this.currGen;
-     }
-
-     public char[][] getTmpGen() {
-         return tmpGen;
-     }
-
-     void generateGeneration(char[][] currentGeneration, int size) {
+    void copyCurrToNext(char[][] currentGeneration, int size) {
         this.currGen = currentGeneration;
         this.nextGen = new char[size][size];
         for (int i = 0; i < currentGeneration.length; i++) {
-            for (int j = 0; j < currentGeneration.length; j++){
+            for (int j = 0; j < currentGeneration.length; j++) {
                 this.nextGen[i][j] = currentGeneration[i][j];
             }
         }
     }
 
-    void evolveOneGeneration() {
+    void createTmpGen() {
         // create new array with added rows on the edges
         this.tmpGen = new char[currGen.length + 2][currGen.length + 2];
 
@@ -169,16 +156,70 @@ public class Main {
 
     }
 
-    void printGenTmp() {
-         System.out.println("TMP GEN");
-         for (int i = 0; i < currGen.length + 2; i++) {
-             for (int j = 0; j < currGen.length + 2; j++) {
-                 System.out.print(tmpGen[i][j] + " ");
-             }
-             System.out.println();
-         }
-     }
+    int countElementNeighbors(char[][] element) {
+        // count alive neighbors - elements with 'o'
+        int count = 0;
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                if (element[k][l] == 'O')
+                    count++;
+            }
+        }
+        return count;
+    }
 
+    // iterate through tmpGen - and count neighbors in each 3x3 element
+    void evolveOneGeneration() {
+        int countOfAlive = 0;
+        for (int i = 1; i < tmpGen.length - 1; i++) {
+            for (int j = 1; j < tmpGen.length - 1; j++) {
+                // fill the element
+                char[][] element = {
+                        {tmpGen[i - 1][j - 1], tmpGen[i - 1][j], tmpGen[i - 1][j + 1]},
+                        {tmpGen[i][j - 1], tmpGen[i][j], tmpGen[i][j + 1]},
+                        {tmpGen[i + 1][j - 1], tmpGen[i + 1][j], tmpGen[i + 1][j + 1]}
+                };
+                System.out.println("This is element created based on [" + i + ", " + j + "] index: ");
+                printOneElement(element);
+
+                countOfAlive = countElementNeighbors(element);
+
+                // logic for progression
+                System.out.println("Alive neighbors: " + countOfAlive);
+                if (countOfAlive == 3) {
+
+                    if (tmpGen[i][j] == '-')
+                        System.out.println("Reborn!");
+                    else
+                        System.out.println("Survives");
+                } else if (countOfAlive == 2) {
+                    System.out.println("Survives");
+                } else {
+                    System.out.println("Dead");
+                }
+
+            }
+        }
+    }
+
+    void printOneElement(char[][] element) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(element[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void printGenTmp() {
+        System.out.println("TMP GEN");
+        for (int i = 0; i < currGen.length + 2; i++) {
+            for (int j = 0; j < currGen.length + 2; j++) {
+                System.out.print(tmpGen[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
 
 
